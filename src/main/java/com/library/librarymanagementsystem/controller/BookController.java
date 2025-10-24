718e67ca78dc39b9022f4f3a8e6488b5f0137928
helisara1 <sarahanapa2005@gmail.com>
2025-10-24 08:04:07 +0530

    Added delete book feature

--- src/main/java/com/library/librarymanagementsystem/controller/BookController.java
+++ src/main/java/com/library/librarymanagementsystem/controller/BookController.java
@@ -3,8 +3,12 @@ package com.library.librarymanagementsystem.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +34,16 @@ public class BookController {
     @PostMapping
     public Book addBook(@RequestBody Book book) {
         return service.addBook(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable Long id) {
+        String result = service.deleteBook(id);
+        if (result.equals("Book deleted successfully")) {
+            return ResponseEntity.ok(result);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+        }
     }
 
     @GetMapping("/search")
